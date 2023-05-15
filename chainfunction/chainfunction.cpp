3844d63d09a443ac53c1a66a7fe9704cf8996821
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-15 16:14:30 +0700

    tambahan buku setJudul(string judul)

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -5,7 +5,14 @@
 using namespace std;
 class buku
 {
+	string judul;
 
+public:
+	buku setJudul(string judul)
+	{
+		this->judul = judul;
+		return *this; // chain function
+	}
 };
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
