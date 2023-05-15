a8ddc6d29077fd26e24f316ffdf391d4aae8a7a6
FarhanVirenze <farhanvirenze18@gmail.com>
2023-05-15 17:04:33 +0700

    tambahn fungsi return 0

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -46,8 +46,11 @@ int main() {
 	angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil
 	delete ptrBelajarcpp; //Destructor Dipanggil
 
-	
-}
+	return 0;
+}//Destructor Dipanggil
+
+
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
